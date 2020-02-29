642177c6d62fb2c5da6aa03ea2fa7524aaa9a98f
Lucas Zamprogno <lucaszamprogno@gmail.com>
2020-02-29 01:48:05 -0800

    Overhaul TransformGrades

--- packages/portal/backend/src-util/TransformGrades.ts
+++ packages/portal/backend/src-util/TransformGrades.ts
@@ -1,3 +1,5 @@
+import * as csvParse from 'csv-parse/lib/sync';
+import * as fs from 'fs-extra';
 import Log, {LogLevel} from "../../../common/Log";
 import Util from "../../../common/Util";
 
@@ -22,6 +24,22 @@ export class TransformGrades {
 
     private dc: DatabaseController;
 
+    // Grade multipliers for public and private test suites
+    private PUB_MULT = 0.5;
+    private PRIV_MULT = 0.5;
+
+    private retroPath = `${__dirname}/retros.csv`;
+    private retroScoreMap: {[student: string]: number};
+
+    private regressionPath = `${__dirname}/regressions.csv`;
+    private regressionScoreMap: {[team: string]: number};
+
+    private parserOptions = {
+        columns:          true,
+        skip_empty_lines: true,
+        trim:             true
+    };
+
     /**
      * Whether the execution is for testing (true) or should actually change the database (false).
      *
@@ -41,15 +59,74 @@ export class TransformGrades {
      *
      * @type {string}
      */
-    private readonly DELIVID: string = 'd3';
+    private readonly DELIVID: string = 'c1';
 
     constructor() {
         Log.info("TransformGrades::<init> - start");
         this.dc = DatabaseController.getInstance();
     }
 
-    public async process(): Promise<void> {
+    public loadRegressionScores() {
+        const csvData = fs.readFileSync(this.regressionPath).toString();
+        const data = csvParse(csvData, this.parserOptions);
+        for (const record of data) {
+            const name = record["Team"];
+            const val = record["Penalties"];
+            this.regressionScoreMap[name] = val;
+        }
+    }
+
+    public applyRegressionScore(grade: Grade, repoId: string) {
+        Log.trace(`Applying retro score to ${grade.personId}. Would be ${this.regressionScoreMap[repoId]}`);
+        grade.score -= this.regressionScoreMap[repoId];
+    }
+
+    public loadRetroScores() {
+        const csvData = fs.readFileSync(this.retroPath).toString();
+        const data = csvParse(csvData, this.parserOptions);
+        for (const record of data) {
+            const member1 = record["Q1.5"];
+            const member1Score = this.getScore(member1, record["Q2.9"]);
+            const member2 = record["Q24"];
+            const member2Score = this.getScore(member1, record["Q35"]);
+            const member3 = record["Q52"];
+            const member3Score = this.getScore(member1, record["Q47"]);
+            this.regressionScoreMap[member1] = member1Score;
+            this.regressionScoreMap[member2] = member2Score;
+            this.regressionScoreMap[member3] = member3Score;
+        }
+    }
+
+    public applyRetroScores(grade: Grade) {
+        const retroScore = this.retroScoreMap[grade.personId];
+        Log.trace(`Applying retro score to ${grade.personId}. Would be ${retroScore}`);
+        grade.custom.c1Retro = retroScore;
+        if (retroScore !== 1.0) {
+            grade.comment = `Retrospective score: ${retroScore}.
+            Note that retrospective scores will not be applied to grades until end of term`;
+        } else {
+            grade.comment = `Retrospective score: 1.0.`;
+        }
+    }
+
+    private getScore(name: string, field: string) {
+        try {
+            return Number(field.split(" ")[0]);
+        } catch (e) {
+            if (name.length > 0) {
+                Log.warn(`Parsing of score ${field} failed for student ${name}`);
+            }
+            return 1;
+        }
+    }
+
+    public async transformGrades(): Promise<void> {
         Log.info("TransformGrades::process() - start for delivId: " + this.DELIVID);
+
+        this.loadRegressionScores();
+        this.loadRetroScores();
+        Log.trace(this.regressionScoreMap);
+        Log.trace(this.retroScoreMap);
 
         const gradesC = new GradesController();
         const resultsC = new ResultsController();
@@ -70,7 +147,7 @@ export class TransformGrades {
         const gradeDeltas: number[] = [];
 
         for (const grade of grades) {
-            const url = grade.URL;
+            const url = grade.URL; // TODO change this one
 
             const result = await resultsC.getResultFromURL(url, this.DELIVID);
             if (result !== null) {
@@ -99,48 +176,18 @@ export class TransformGrades {
                 }
 
                 const scorePub = Number(result.output.report.scoreTest);
-                const scoreCover = Number(result.output.report.scoreCover);
                 const scorePriv = Number((result.output.report.custom as any).private.scoreTest);
                 const scorePubOverall = Number(result.output.report.scoreOverall);
 
-                let finalScore = 0;
-                if (this.DELIVID === 'd1') {
-                    // 25% private tests
-                    finalScore = (((scorePub * .75) + (scorePriv * .25)) * .8) + (scoreCover * .2);
-                    finalScore = Number(finalScore.toFixed(2));
-                    Log.info("Updating grade for " + this.DELIVID + "; original: " +
+                let finalScore = (scorePub * this.PUB_MULT) + (scorePriv * this.PRIV_MULT);
+                finalScore = Number(finalScore.toFixed(2));
+                Log.info("Updating grade for " + this.DELIVID + "; original: " +
+                    scorePubOverall.toFixed(0) + "; new: " + finalScore.toFixed(0));
+
+                // if there's a big difference, print a warning
+                if ((scorePub - scorePriv) > 20) {
+                    Log.warn("Divergent score between public and private; original: " +
                         scorePubOverall.toFixed(0) + "; new: " + finalScore.toFixed(0));
-
-                    // if there's a big difference, print a warning
-                    if ((scorePub - scorePriv) > 20) {
-                        Log.warn("Divergent score between public and private; original: " +
-                            scorePubOverall.toFixed(0) + "; new: " + finalScore.toFixed(0));
-                    }
-                }
-                if (this.DELIVID === 'd2') {
-                    // 25% private tests
-                    finalScore = (((scorePub * .75) + (scorePriv * .25)) * .8) + (scoreCover * .2);
-                    finalScore = Number(finalScore.toFixed(2));
-                    Log.info("Updating grade for " + this.DELIVID + "; original: " +
-                        scorePubOverall + "; new: " + finalScore);
-
-                    // if there's a big difference, print a warning
-                    if ((scorePub - scorePriv) > 20) {
-                        Log.warn("Divergent score between public and private; original: " +
-                            scorePubOverall + "; new: " + finalScore);
-                    }
-                } else if (this.DELIVID === 'd3') {
-                    // 50% private tests
-                    finalScore = (((scorePub * .5) + (scorePriv * .5)) * .8) + (scoreCover * .2);
-                    finalScore = Number(finalScore.toFixed(2));
-                    Log.info("Updating grade for " + this.DELIVID + "; original: " +
-                        scorePubOverall.toFixed(0) + "; new: " + finalScore.toFixed(0));
-
-                    // if there's a big difference, print a warning
-                    if ((scorePub - scorePriv) > 20) {
-                        Log.warn("Divergent score between public and private; original: " +
-                            scorePubOverall.toFixed(0) + "; new: " + finalScore.toFixed(0));
-                    }
                 }
 
                 const newGrade: Grade = JSON.parse(JSON.stringify(grade)); // Object.assign is a shallow copy which doesn't work here
@@ -151,6 +198,9 @@ export class TransformGrades {
                 // could add comment here too if needed (e.g., to newGrade.comment)
                 newGrade.urlName = "Transformed";
                 newGrade.score = finalScore;
+
+                this.applyRegressionScore(newGrade, result.repoId);
+                this.applyRetroScores(newGrade);
 
                 gradeDeltas.push(Number((newGrade.score - grade.score).toFixed(2))); // track delta
 
@@ -201,10 +251,10 @@ export class TransformGrades {
     }
 }
 
-const ppt = new TransformGrades();
+const transformer = new TransformGrades();
 const start = Date.now();
 Log.Level = LogLevel.INFO;
-ppt.process().then(function() {
+transformer.transformGrades().then(function() {
     Log.info("TransformGrades::process() - complete; took: " + Util.took(start));
     process.exit();
 }).catch(function(err) {
